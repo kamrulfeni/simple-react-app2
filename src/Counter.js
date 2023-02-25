acf889060787d8609cc02f17a5fa413fe5fde96f
kamrulHasan <kamrulhasandhaka704@gmail.com>
2023-02-25 15:31:43 +0600

    my simple react app

---
+++ src/Counter.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+    const [count, setCount] = useState(1);
+
+    const increament = () => {
+        setCount((prevCount) => prevCount * 2);
+    };
+    const decreament = () => {
+        setCount((prevCount) => prevCount / 2);
+    };
+
+    return (
+        <div
+            class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
+        >
+            <div class="text-2xl font-semibold">{count}</div>
+            <div class="flex space-x-3">
+                <button
+                    class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+                    onClick={increament}
+                >
+                    Increment
+                </button>
+                <button
+                    class="bg-red-400 text-white px-3 py-2 rounded shadow"
+                    onClick={decreament}
+                >
+                    Decrement
+                </button>
+            </div>
+        </div>
+
+    );
+};
+
+export default Counter;
